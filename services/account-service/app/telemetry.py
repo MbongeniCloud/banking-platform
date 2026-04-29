fa68acbafdc761ebfeaed1c1bcaad3f0631665b8
MbongeniCloud <Lefotlhem@yahoo.com>
2026-04-29 01:37:50 -0700

    feat: wire Application Insights distributed tracing into all services

--- services/account-service/app/telemetry.py
+++ services/account-service/app/telemetry.py
@@ -1,13 +1,27 @@
 import os
 from fastapi import FastAPI
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.fastapi.fastapi_middleware import FastAPIMiddleware
+import logging
 
 def setup_telemetry(app: FastAPI):
     connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
     if not connection_string:
         return
+
     try:
-        from opencensus.ext.azure.trace_exporter import AzureExporter
-        from opencensus.trace.samplers import ProbabilitySampler
-        from opencensus.trace.tracer import Tracer
+        app.add_middleware(
+            FastAPIMiddleware,
+            exporter=AzureExporter(connection_string=connection_string),
+            sampler=ProbabilitySampler(1.0),
+        )
+
+        logger = logging.getLogger("account-service")
+        logger.addHandler(AzureLogHandler(connection_string=connection_string))
+        logger.setLevel(logging.INFO)
+
     except ImportError:
         return
